a389b212d73b04dd138c845d6dfa2c299ed1c633
JavaByJules <142684618+JavaByJules@users.noreply.github.com>
2023-08-20 20:42:11 +0100

    Update Exercise003 and Exercise 003Test
    Now able to output string of flavours. Also added extra test (annotated as such) to test for existence of a flavour not in array

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -4,13 +4,11 @@ import java.util.Arrays;
 public class Exercise003 {
 
     int getIceCreamCode(String iceCreamFlavour) {
-        //return findIndex(iceCreamFlavour, 0);
         return Arrays.asList(iceCreamFlavours).indexOf(iceCreamFlavour);
     }
 
     String[] iceCreamFlavours () {
-        //return Arrays.stream(iceCreamFlavours);
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        return iceCreamFlavours;
     }
 
     String[] iceCreamFlavours = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
--- src/test/java/com/techreturners/exercise003/Exercise003Test.java
+++ src/test/java/com/techreturners/exercise003/Exercise003Test.java
@@ -1,7 +1,6 @@
 package com.techreturners.exercise003;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -40,14 +39,21 @@ public class Exercise003Test {
         assertEquals(expected, ex003.getIceCreamCode(iceCreamFlavour));
     }
 
+    // added by Jules - see what happens if a flavour not in the array of flavours is input
     @Test
-    @Disabled
+    public void checkGetIceCreamCodeForStrawberry() {
+        String iceCreamFlavour = "Strawberry";
+        int expected = -1;
+
+        assertEquals(expected, ex003.getIceCreamCode(iceCreamFlavour));
+    }
+
+    @Test
     public void checkPickMultipleIceCreamFlavours() {
 
         String[] expected = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
 
         assertArrayEquals(expected, ex003.iceCreamFlavours());
     }
-
 
 }
