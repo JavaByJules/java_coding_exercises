189872eaa843ce6764c8bb0c6f8adf4603b24cbc
JavaByJules <142684618+JavaByJules@users.noreply.github.com>
2023-08-20 11:51:51 +0100

    Exercise 2
    Successful completion of Exercise 2

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -1,14 +1,20 @@
 package com.techreturners.exercise002;
 
+import com.techreturners.exercise001.User;
+
+import java.util.List;
+
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        // Add your code here
-        return false;
+    public boolean isFromManchester(Person person ) {
+        return person.city.equals("Manchester");
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
+    public boolean canWatchFilm(Person person, int ageLimit) {
+        if (person.age >= ageLimit)
+        {
+            return true;
+        }
         return false;
     }
     
