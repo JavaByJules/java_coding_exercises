259371a7920ce9fda8778bd574d322aa529d6cab
JavaByJules <142684618+JavaByJules@users.noreply.github.com>
2023-08-20 11:51:05 +0100

    Create Person.java

---
+++ src/main/java/com/techreturners/exercise002/Person.java
@@ -0,0 +1,16 @@
+package com.techreturners.exercise002;
+
+public class Person {
+
+        protected String firstName;
+        protected String lastName;
+        protected String city;
+        protected Integer age;
+
+        public Person(String firstName, String lastName, String city, Integer age) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.city = city;
+            this.age = age;
+        }
+}
