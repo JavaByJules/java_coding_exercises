3405f14e75ec9224a873378152b4e4f6c9bd2ce7
JavaByJules <142684618+JavaByJules@users.noreply.github.com>
2023-08-25 10:16:05 +0100

    Exercise 5
    Finished refactoring Exercise 5, per Simon's advice. Excess code removed, 3 new tests added (at bottom of test script)

--- src/test/java/com/techreturners/exercise005/Exercise005Test.java
+++ src/test/java/com/techreturners/exercise005/Exercise005Test.java
@@ -21,52 +21,68 @@ public class Exercise005Test {
         assertFalse(ex005.isPangram(""));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
     @Test
     public void checkPerfectLowerCaseSentenceIsAPangram() {
         assertTrue(ex005.isPangram("abcdefghijklmnopqrstuvwxyz"));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
     @Test
     public void checkPerfectUpperCaseSentenceIsAPangram() {
         assertTrue(ex005.isPangram("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
     @Test
     public void checkSentenceWithLowerCaseIsAPangram() {
         assertTrue(ex005.isPangram("the quick brown fox jumps over the lazy dog"));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
+
     @Test
     public void checkSentenceWithMixedUpperAndLowerCaseIsAPangram() {
         assertTrue(ex005.isPangram("The Five boXing wiZards Jump QuicklY"));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
     @Test
     public void checkSentenceWithMissingLetterXIsNotAPangram() {
         assertFalse(ex005.isPangram("the quick brown fo jumps over the lazy dog"));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
+
     @Test
     public void checkSentenceWithMissingLetterHIsNotAPangram() {
         assertFalse(ex005.isPangram("five boxing wizards jump quickly at it"));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
+
     @Test
     public void checkSentenceWithUnderscoresIsAPangram() {
         assertTrue(ex005.isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog"));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
+
     @Test
     public void checkSentenceWithNumbersIsAPangram() {
         assertTrue(ex005.isPangram("the 1 quick brown fox jumps over the 2 lazy dogs"));
     }
+
+    // The below tests have been added by me
+    // The first test checks for the quick brown fox phrase but in upper case
+    @Test
+    public void checkSentenceWithUpperCaseIsAPangram() {
+        assertTrue(ex005.isPangram("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"));
+    }
+
+
+    // In the below test, the 'z' is misssing
+    @Test
+    public void checkImperfectLowerCaseOneLetterMissing() {
+        assertFalse(ex005.isPangram("abcdefghijklmnopqrstuvwxy"));
+    }
+    // In the below test, the 'A' is missing
+    @Test
+    public void checkImperfectUpperCaseOneLetterMissing() {
+        assertFalse(ex005.isPangram("BCDEFGHIJKLMNOPQRSTUVWXYZ"));
+    }
+
 
 }
