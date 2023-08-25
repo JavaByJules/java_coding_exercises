8b6a8ee0c9129a5c47f36b5c6b059c90abcd67fd
JavaByJules <142684618+JavaByJules@users.noreply.github.com>
2023-08-25 12:06:38 +0100

    Exercise 004
    Got Gigasecond program to work correctly against tests, including additional test to calculate Gigasecond since the exact Date and Time of my Birth

--- src/main/java/com/techreturners/exercise004/Exercise004.java
+++ src/main/java/com/techreturners/exercise004/Exercise004.java
@@ -5,25 +5,17 @@ import java.time.LocalDateTime;
 
 public class Exercise004 {
 
-    // I have to confess, I have not heard of a gigasecond before. Anyway, what I would do,
-    // bearing in mind the amount of days in a year can vary, as can the amount of days in
-    // a month, my formula would be as follows:
-    // Use % to mod the amount of whole days, then the same process to find the amount of
-    // whole hours, then the same to identify the remaining minutes and seconds.
-    // The formula to calculate the number of seconds in a day would be to find the value
-    // of 60 (minutes) * 60 (seconds) * 24 (hours). Once this is identified, we can then
-    // identify the amount of whole days in a gigasecond. Then, we can identify whole hours
-    // 60 (minutes) * 60 (seconds), the whole minutes (60 seconds) and any remaining seconds
+    private LocalDateTime dateTime;
 
     public Exercise004(LocalDate date) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        dateTime = LocalDateTime.from(date.atTime(0, 0));
     }
 
     public Exercise004(LocalDateTime dateTime) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        this.dateTime = dateTime;
     }
 
     public LocalDateTime getDateTime() {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        return this.dateTime.plusSeconds(1000000000);
     }
 }
--- src/test/java/com/techreturners/exercise004/Exercise004Test.java
+++ src/test/java/com/techreturners/exercise004/Exercise004Test.java
@@ -18,7 +18,6 @@ public class Exercise004Test {
         assertEquals(expected, ex004.getDateTime());
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
     @Test
     public void checkGetDateTimeWhenBothDateAndTimeIsSpecified() {
 
@@ -28,12 +27,22 @@ public class Exercise004Test {
         assertEquals(expected, ex004.getDateTime());
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
     @Test
     public void checkGetDateTimeWhenBothDateAndTimeIsSpecifiedWithDayRollOver() {
 
         Exercise004 ex004 = new Exercise004(LocalDateTime.of(2021, Month.JANUARY, 24, 23, 59, 59, 0));
         LocalDateTime expected = LocalDateTime.of(2052, Month.OCTOBER, 03, 1, 46, 39);
+
+        assertEquals(expected, ex004.getDateTime());
+    }
+
+    // Test to see what is a Gigasecond from my Date of Birth, 24th October 1968
+    @Test
+    public void checkGetDateTimeForMyDateOfBirth() {
+
+        Exercise004 ex004 = new Exercise004(LocalDateTime.of(1968, Month.OCTOBER, 24, 5, 6, 0, 0));
+
+        LocalDateTime expected = LocalDateTime.of(2000, Month.JULY, 2, 6, 52, 40);
 
         assertEquals(expected, ex004.getDateTime());
     }
